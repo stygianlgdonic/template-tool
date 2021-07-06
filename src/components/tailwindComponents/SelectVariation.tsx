e2218e0760c1d82f110703a6b42a277865d8dd5a
stygianlgdonic <stygianlgdonic@gmail.com>
2021-07-06 04:28:51 -0700

    variationindex

--- src/components/tailwindComponents/SelectVariation.tsx
+++ src/components/tailwindComponents/SelectVariation.tsx
@@ -2,18 +2,13 @@ import React, { useState } from 'react'
 
 const SelectVariation = ({ variations, variationIndex, setVariationIndex }) => {
 
-    const [displayChoices, setDisplayChoices] = useState(false)
-
     return (
         <div className="flex">
             {variations.map((item, index) => (
                 <p
                     key={index}
-                    className="text-red-100 block px-4 py-2 mr-1 ml-1 text-sm cursor-pointer bg-red-700 hover:bg-red-800"
-                    onClick={() => {
-                        setVariationIndex(index)
-                        setDisplayChoices(false)
-                    }}
+                    className={"text-red-100 block px-4 py-2 mr-1 ml-1 text-sm cursor-pointer rounded border border-gray-900"}
+                    onClick={() => setVariationIndex(index)}
                     role="menuitem"
                     tabIndex={-1}
                 >{item.name}</p>
