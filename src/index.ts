2f0c90b7c4d7c1abbd68d27bbffe863bb8dd7f48
stygianlgdonic <stygianlgdonic@gmail.com>
2021-07-19 15:48:18 +0500

    server

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,10 @@ if (module.hot) {
     console.info('✅  Server-side HMR Enabled!');
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 export default express()
     .use((req, res) => app.handle(req, res))
-    .listen(port, () => {
+    .listen(port, "0.0.0.0", () => {
         console.log(`> App started http://localhost:${port}`)
     });
