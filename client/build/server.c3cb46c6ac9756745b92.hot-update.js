2230d4b0bfda54d81ff4a61ecbe74e7a358a9d29
stygianlgdonic <stygianlgdonic@gmail.com>
2021-07-29 18:40:21 +0500

    lazy loading done

---
+++ client/build/server.c3cb46c6ac9756745b92.hot-update.js
@@ -0,0 +1,118 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "./src/App.tsx":
+/*!*********************!*\
+  !*** ./src/App.tsx ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var tailwindcss_tailwind_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! tailwindcss/tailwind.css */ "./node_modules/tailwindcss/tailwind.css");
+/* harmony import */ var _NoSSR__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./NoSSR */ "./src/NoSSR.tsx");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-router-dom */ "react-router-dom");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_router_dom__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _App_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./App.css */ "./src/App.css");
+/* harmony import */ var _contexts_DesignToolContext__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./contexts/DesignToolContext */ "./src/contexts/DesignToolContext.tsx");
+/* harmony import */ var _contexts_TemplateContext__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./contexts/TemplateContext */ "./src/contexts/TemplateContext.tsx");
+var _jsxFileName = "D:\\dev\\cardclan-backend\\client\\src\\App.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+
+
+
+const Dashboard = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.lazy(() => __webpack_require__.e(/*! import() */ 1).then(__webpack_require__.bind(null, /*! ./Screens/Dashboard/Dashboard */ "./src/Screens/Dashboard/Dashboard.tsx")));
+const CreateCardLayout = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.lazy(() => __webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! ./../src/Screens/CreateCardLayout/CreateCardLayout */ "./src/Screens/CreateCardLayout/CreateCardLayout.tsx")));
+
+const App = () => {
+  return __jsx(_NoSSR__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17,
+      columnNumber: 9
+    }
+  }, __jsx(Suspense, {
+    fallback: __jsx(Dashboard, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 19,
+        columnNumber: 17
+      }
+    }),
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18,
+      columnNumber: 13
+    }
+  }, __jsx(react_router_dom__WEBPACK_IMPORTED_MODULE_3__["Routes"], {
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21,
+      columnNumber: 17
+    }
+  }, __jsx(react_router_dom__WEBPACK_IMPORTED_MODULE_3__["Route"], {
+    path: "/",
+    element: __jsx(Dashboard, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 23,
+        columnNumber: 46
+      }
+    }),
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23,
+      columnNumber: 21
+    }
+  }), __jsx(_contexts_DesignToolContext__WEBPACK_IMPORTED_MODULE_5__["DesignToolProvider"], {
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25,
+      columnNumber: 21
+    }
+  }, __jsx(_contexts_TemplateContext__WEBPACK_IMPORTED_MODULE_6__["TemplateProvider"], {
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26,
+      columnNumber: 25
+    }
+  }, __jsx(react_router_dom__WEBPACK_IMPORTED_MODULE_3__["Route"], {
+    path: "createcard",
+    element: __jsx(CreateCardLayout, {
+      __self: undefined,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 27,
+        columnNumber: 63
+      }
+    }),
+    __self: undefined,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27,
+      columnNumber: 29
+    }
+  }))))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (App);
+
+/***/ })
+
+};
+//# sourceMappingURL=server.c3cb46c6ac9756745b92.hot-update.js.map
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,38 @@
 import React, { Suspense } from 'react';
 import "tailwindcss/tailwind.css"
 import NoSSR from './NoSSR'
-import { BrowserRouter, useRoutes, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import './App.css';
-// const Dashboard = React.lazy(() => import('./Screens/Dashboard/Dashboard'));
-import Dashboard from './../src/Screens/Dashboard/Dashboard';
-import CreateCardLayout from './../src/Screens/CreateCardLayout/CreateCardLayout';
-import Centerboard from './components/Centerboard/Centerboard';
-import LeftSidebar from './components/LeftSideBar/LeftSidebar';
 import { DesignToolProvider } from './contexts/DesignToolContext';
 import { TemplateProvider } from './contexts/TemplateContext';
-// const CreateCardLayout = React.lazy(() => import('./../src/Screens/CreateCardLayout/CreateCardLayout'));
 
-// const CreateCardLayout = React.lazy(() => import('./Screens/CreateCardLayout/CreateCardLayout'));
+import Dashboard from './Screens/Dashboard/Dashboard'
+const CreateCardLayout = React.lazy(() => import('./../src/Screens/CreateCardLayout/CreateCardLayout'));
 
 const App = () => {
 
 
     return (
-        // <Suspense fallback={
-        //     <Dashboard />
-        // }>
 
         <NoSSR >
             <Routes>
 
                 <Route path="/" element={<Dashboard />}>
-                    <Route path="/sss" element={<Centerboard />} />
-                    <Route path="/404" element={<LeftSidebar />} />
-
                 </Route>
+
                 <DesignToolProvider>
                     <TemplateProvider>
-
-
-                        <Route path="createcard" element={<CreateCardLayout />}>
-                            <Route path="/sss" element={<Centerboard />} />
-                            <Route path="/404" element={<LeftSidebar />} />
-
-                        </Route>
+                        <Suspense fallback={
+                            <p>Loading ...</p>
+                        }>
+                            <Route path="createcard" element={<CreateCardLayout />}>
+                            </Route>
+                        </Suspense>
                     </TemplateProvider>
                 </DesignToolProvider>
 
             </Routes>
         </NoSSR>
-        // </Suspense>
     );
 }
 
