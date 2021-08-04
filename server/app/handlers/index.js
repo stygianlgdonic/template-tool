4d1561c7241f1ea2e3b53d2119426e8fe5de42bf
wakas <wakasabbasid@gmail.com>
2021-08-04 15:46:25 +0500

    added Card CRUD

--- server/app/handlers/index.js
+++ server/app/handlers/index.js
@@ -2,3 +2,4 @@ exports.errorHandler = require("./error");
 exports.thingHandler = require("./thing");
 exports.thingsHandler = require("./things");
 exports.templateHandler = require("./template")
+exports.cardHandler = require("./card")
