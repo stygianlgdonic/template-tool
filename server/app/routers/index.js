87f0fa45b89cc6d4184d994ff58478c5ae917d96
wakas <wakasabbasid@gmail.com>
2021-08-06 17:10:49 +0500

    implemented login and signup flow

--- server/app/routers/index.js
+++ server/app/routers/index.js
@@ -2,3 +2,6 @@ exports.thingsRouter = require("./things");
 exports.templateRouter = require("./template");
 exports.templateCategoryRouter = require("./templatecategory");
 exports.cardRouter = require("./card");
+exports.userRouter = require("./user");
+exports.secureRouter = require("./secure-routes");
+
