fb2388b5cdf80857ed22012b60cb8ebc7a12a56c
wajix0078 <wajahatmasood886@gmail.com>
2021-08-20 20:02:40 +0500

    New commit

---
+++ client/build/server.282363257374b6dff1d2.hot-update.js
@@ -0,0 +1,16 @@
+exports.id = "server";
+exports.modules = {
+
+/***/ "react-color/lib/components/common":
+/*!****************************************************!*\
+  !*** external "react-color/lib/components/common" ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react-color/lib/components/common");
+
+/***/ })
+
+};
+//# sourceMappingURL=server.282363257374b6dff1d2.hot-update.js.map
